c2df0358e969c48664bb57916c9ac8d0b95f310a
OvoPraise <ovopraise05@gmail.com>
2024-08-21 15:46:04 +0100

    Changes background to blue

--- pythoncode.py
+++ pythoncode.py
@@ -6,7 +6,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, font=("Arial Black",78,"bold"), bg="darkgreen", fg="pink")
+label = Label(window, font=("Arial Black",78,"bold"), bg="darkblue", fg="pink")
 label.pack(pady=100)
 
 def clock():
